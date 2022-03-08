49c84abbf1bd4aaaf7423b801463d4aca385b651
moniem123 <mohammedabdelmoniem65@gmail.com>
2022-03-09 01:49:55 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import {router} from './routers.js'
 
 
 // window.base_url = 'http://localhost:3000';
-window.base_url = 'https://mo-resto.herokuapp.com/api';
+window.base_url = 'https://mo-resto.herokuapp.com';
 
 const app = createApp(App);
 app.use(router);
