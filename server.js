0745e1ed9ba41c392528e93c4e055aca5f806c00
moniem123 <mohammedabdelmoniem65@gmail.com>
2022-03-08 06:06:22 +0300

    server

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express')
-const serverStatic = require('server-static')
+const serverStatic = require('serve-static')
 const path = require('path')
 
 const app = express();
-
 app.use('/',serverStatic(path.join(__dirname,'/public')))
 
 const port = process.env.PORT || 8000
 
 app.listen(port)
 
-console.log('listen on posrt : '+ port);
+console.log('listen on port : '+ port);
