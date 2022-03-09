3a669cc6458918d58e854f34461a466e1e7b1152
moniem123 <mohammedabdelmoniem65@gmail.com>
2022-03-09 04:23:19 +0300

    build app adn add json server

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
-const express = require('express')
+// const express = require('express')
 const serverStatic = require('serve-static')
 const path = require('path')
+// const app = express();
+// app.use('/',serverStatic(path.join(__dirname,'/dist')))
 
-const app = express();
-app.use('/',serverStatic(path.join(__dirname,'/dist')))
+// const port = process.env.PORT || 8080
 
-const port = process.env.PORT || 8080
+// app.listen(port)
 
-app.listen(port)
+// console.log('listen on port : '+ port);
 
-console.log('listen on port : '+ port);
+
+const data = require('./db.json');
+const authServer = require('json-server-auth');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router(data);
+const middlewares = jsonServer.defaults();
+const db_port = process.env.PORT || 8282;
+server.use(authServer);
+
+// server.use(jsonServer.rewriter({
+//   '/api/*': '/$1',
+// }))
+server.use('/',serverStatic(path.join(__dirname,'/dist')));
+server.use(router);
+server.listen(db_port, () => {
+  console.log('data is running on port : '+db_port);
+});
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import {router} from './routers.js'
 
 
-// window.base_url = 'http://localhost:3000';
+// window.base_url = 'http://localhost:8282';
 window.base_url = 'https://mo-resto.herokuapp.com';
 
 const app = createApp(App);
