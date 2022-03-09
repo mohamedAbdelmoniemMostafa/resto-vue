2cbc755be7b06e37ee6c2f425163677ff645a083
moniem123 <mohammedabdelmoniem65@gmail.com>
2022-03-09 05:20:00 +0300

    serve

--- local_server.js
+++ local_server.js
@@ -1,15 +1,8 @@
-const express = require('express')
+// const express = require('express')
 const serverStatic = require('serve-static')
 const path = require('path')
-const app = express();
-app.use('/',serverStatic(path.join(__dirname,'/dist')))
-
-const port = process.env.PORT || 8080
-
-app.listen(port)
-
-console.log('listen on port : '+ port);
-
+// const app = express();
+// app.use('/',serverStatic(path.join(__dirname,'/dist')))
 
 const data = require('./db.json');
 const authServer = require('json-server-auth');
@@ -17,15 +10,15 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router(data);
 // const middlewares = jsonServer.defaults();
-// const db_port = process.env.PORT || 8282;
+const db_port = process.env.PORT || 8282;
 server.db = router.db
 server.use(authServer);
 
 // server.use(jsonServer.rewriter({
 //   '/api/*': '/$1',
 // }))
-// server.use('/',serverStatic(path.join(__dirname,'/dist')));
+server.use('/',serverStatic(path.join(__dirname,'/dist')));
 server.use(router);
-// server.listen(db_port, () => {
-//   console.log('data is running on port : '+db_port);
-// });
+server.listen(db_port, () => {
+  console.log('data is running on port : '+db_port);
+});
